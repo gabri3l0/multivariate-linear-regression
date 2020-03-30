ba0587a6a6fe0a41fdff00878559d75a1d586f17
Gabriel <gabriel.lopez@udem.edu>
2020-03-30 02:28:36 -0600

    Add comments to main file

--- multivariate-linear-regression.py
+++ multivariate-linear-regression.py
@@ -9,24 +9,46 @@
 """
 
 def main():
+	"""
+	Aqui se manda llamar el archivo para leer los datos del CSV, asi mismo
+	se obtienen las x, y de entrenamiento, el promedio de las x aplicando 
+	escalamiento de caracteristicas y la desviacion estandar para despues
+	obtener los parametros w y con base eso usar los datos de prueba y
+	predecir las y o el costo de la ultima milla
+
+	Datos de entrada:
+	Nada
+
+	Datos de salida:
+	Nada
+	"""
+	# Importa las librerias estandard y la libreria utilityfunctions
 	import numpy as np
 	import utilityfunctions as uf
 
+	# Metodo para obtener el x,y de entrenamiento, promedio, desviacion estandar, y caracteristicas
 	x_train, y_train, mean, std, features = uf.load_data('training-data-multivariate.csv')
 
+	# Se inicializa los hiperparametros
 	learning_rate = 0.5
 	stopping_criteria = 0.01
 
+	# Inicializa w
 	w = (np.array([[0.0]*features]).T)
 
+	# Metodo para obtener el gradiente descendiente
 	w = uf.gradient_descent(x_train, y_train, w, stopping_criteria, learning_rate)
 
+	# Metodo para imprimir las w optimas
 	uf.show_w(w)
 
+	# Metodo para cargar los datos de pruebas
 	x_testing = uf.load_predict('predict_last_mile.csv',mean,std)
 
+	# Metodo para obtener el costo de la ultima milla de los datos de prueba
 	price = uf.predict_last_mile(x_testing,w)
 
+	# Metodo para imprimir el costo de la ultima milla de los datos de prueba
 	uf.show_last(price)
-	
+
 main()
